02089297fee48dcf0f2d8b371f7b160f69598580
ZXCVestec <semabersenev@gmail.com>
2024-11-08 23:00:16 +0700

    dz

--- Program.cs
+++ Program.cs
@@ -10,7 +10,84 @@ namespace DZ09_11
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1");
+            //Задание 1
+            Console.WriteLine("Задание 1");
+            Console.WriteLine(Math.Round(Math.E, 1));
+            Console.ReadLine();
+            //Задание 2
+            Console.WriteLine("Задание 2");
+            Console.WriteLine("10\n50");
+            Console.ReadLine();
+            //Задание 3
+            Console.WriteLine("Задание 3");
+            Console.WriteLine("1\n2\n3\n4");
+            Console.ReadLine();
+            //Задание 4
+            Console.WriteLine("Задание 4");
+            string a = Console.ReadLine();
+            int b = Convert.ToInt32(a);
+            Console.WriteLine(b + 10);
+            Console.ReadLine();
+            //Задание 5
+            Console.WriteLine("Задание 5");
+            a = Console.ReadLine();
+            b = Convert.ToInt32(a);
+            Console.WriteLine(b * 4);
+            Console.ReadLine();
+            //Задание 6
+            Console.WriteLine("Задание 6");
+            a = Console.ReadLine();
+            b = Convert.ToInt32(a);
+            Console.WriteLine(2 * Math.PI * b);
+            Console.WriteLine(b * Math.PI * b);
+            Console.ReadLine();
+            //Задание 7
+            Console.WriteLine("Задание 7");
+            a = Console.ReadLine();
+            b = Convert.ToInt32(a);
+            Console.WriteLine(Math.Cos(b));
+            Console.ReadLine();
+            //Задание 8
+            Console.WriteLine("Задание 8");
+            a = Console.ReadLine();
+            string c = Console.ReadLine();
+            string e = Console.ReadLine();
+            b = Convert.ToInt32(a);
+            int d = Convert.ToInt32(c);
+            int f = Convert.ToInt32(e);
+            Console.WriteLine(d + b + 2 * Math.Sqrt(f * f + Math.Abs(b - d) * Math.Abs(b - d)));
+            Console.ReadLine();
+            //Задание 9
+            Console.WriteLine("Задание 9");
+
+            Console.ReadLine();
+            //Задание 10
+            //Задание 11
+            //Задание 12
+            //Задание 13
+            //Задание 14
+            //Задание 15
+            //Задание 16
+            //Задание 17
+            //Задание 18
+            //Задание 19
+            //Задание 20
+            //Задание 21
+            //Задание 22
+            //Задание 23
+            //Задание 24
+            //Задание 25
+            //Задание 26
+            //Задание 27
+            //Задание 28
+            //Задание 29
+            //Задание 30
+            //Задание 31
+            //Задание 32
+            //Задание 33
+            //Задание 34
+            //Задание 35
+            //Задание 36
             Console.ReadLine();
         }
     }
